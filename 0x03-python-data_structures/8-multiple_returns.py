ac2ce8969c036cb2856d1cd6fddb81cebe9cc24b
Chukwudalu Ayika <54899981+zemarr@users.noreply.github.com>
2023-01-10 14:30:48 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-	if sentence != '':
-		first_char = sentence[0]
-	else:
-		first_char = None
-	return (len(sentence), first_char)
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
